d58fd03f337d45970795ca393bb9e0d57d233646
Khizar Nawab <khizarnawab1@outlook.com>
2026-01-10 21:06:43 +0500

    refactor: move code to separate files

---
+++ login_handler.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("Need a username to login!")
+	} else if len(cmd.args) > 1 {
+		return fmt.Errorf("Too many arguments")
+	}
+
+	err := s.cfg.SetUser(cmd.args[0])
+	if err != nil {
+		return err
+	}
+	fmt.Printf("User successfully set to %s\n", cmd.args[0])
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -10,32 +9,6 @@ import (
 
 type state struct {
 	cfg *config.Config
-}
-
-type command struct {
-	name string
-	args []string
-}
-
-type commands struct {
-	c map[string]func(*state, command) error
-}
-
-func (c *commands) run(s *state, cmd command) error {
-	val, ok := c.c[cmd.name]
-	if !ok {
-		return fmt.Errorf("Command does not exist")
-	}
-
-	err := val(s, cmd)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (c *commands) register(name string, f func(*state, command) error) {
-	c.c[name] = f
 }
 
 func main() {
@@ -59,40 +32,18 @@ func main() {
 	// fetch CLI arguments
 	args := os.Args
 	if len(args) < 2 {
-		fmt.Println("Insufficient arguments!")
-		os.Exit(1)
+		log.Fatal("Insufficient arguments!")
 	}
 
 	// prepare to execute command by parsing arguments
 	cmd := command{
 		name: args[1],
-	}
-	if len(args) > 2 {
-		cmd.args = args[2:]
-	} else {
-		cmd.args = nil
+		args: args[2:],
 	}
 
 	// run the command
 	err = commands.run(&s, cmd)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-}
-
-func handlerLogin(s *state, cmd command) error {
-	if len(cmd.args) == 0 {
-		return fmt.Errorf("Need a username to login!")
-	} else if len(cmd.args) > 1 {
-		return fmt.Errorf("Too many arguments")
-	}
-
-	err := s.cfg.SetUser(cmd.args[0])
-	if err != nil {
-		return err
-	}
-	fmt.Printf("User successfully set to %s\n", cmd.args[0])
-
-	return nil
 }
