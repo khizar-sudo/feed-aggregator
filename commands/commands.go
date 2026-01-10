39477fee0cde6ba5210ebee792a20cfb7b25ea10
Khizar Nawab <khizarnawab1@outlook.com>
2026-01-11 00:42:50 +0500

    refactor: move commands to separate folder

---
+++ commands/commands.go
@@ -0,0 +1,88 @@
+package commands
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/khizar-sudo/feed-aggregator/internal/config"
+	"github.com/khizar-sudo/feed-aggregator/internal/database"
+	_ "github.com/lib/pq"
+)
+
+type state struct {
+	cfg *config.Config
+	db  *database.Queries
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	c map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	val, ok := c.c[cmd.name]
+	if !ok {
+		return fmt.Errorf("Command does not exist")
+	}
+
+	err := val(s, cmd)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.c[name] = f
+}
+
+func Init() {
+	// initialise json config
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+
+	//initialise database and state (pointer to config and database)
+	db, err := sql.Open("postgres", cfg.DbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+	s := state{
+		cfg: &cfg,
+		db:  dbQueries,
+	}
+
+	// initialise the map of commands
+	commands := commands{
+		c: make(map[string]func(*state, command) error),
+	}
+	commands.register("login", handlerLogin)
+	commands.register("register", handlerRegister)
+
+	// fetch CLI arguments
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatal("Insufficient arguments!")
+	}
+
+	// prepare to execute command by parsing arguments
+	cmd := command{
+		name: args[1],
+		args: args[2:],
+	}
+
+	// run the command
+	err = commands.run(&s, cmd)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,61 +1,9 @@
 package main
 
 import (
-	"database/sql"
-	"log"
-	"os"
-
-	"github.com/khizar-sudo/feed-aggregator/internal/config"
-	"github.com/khizar-sudo/feed-aggregator/internal/database"
-	_ "github.com/lib/pq"
+	"github.com/khizar-sudo/feed-aggregator/commands"
 )
 
-type state struct {
-	cfg *config.Config
-	db  *database.Queries
-}
-
 func main() {
-	// initialise json config
-	cfg, err := config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
-	}
-
-	//initialise database and state (pointer to config and database)
-	db, err := sql.Open("postgres", cfg.DbUrl)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	dbQueries := database.New(db)
-	s := state{
-		cfg: &cfg,
-		db:  dbQueries,
-	}
-
-	// initialise the map of commands
-	commands := commands{
-		c: make(map[string]func(*state, command) error),
-	}
-	commands.register("login", handlerLogin)
-	commands.register("register", handlerRegister)
-
-	// fetch CLI arguments
-	args := os.Args
-	if len(args) < 2 {
-		log.Fatal("Insufficient arguments!")
-	}
-
-	// prepare to execute command by parsing arguments
-	cmd := command{
-		name: args[1],
-		args: args[2:],
-	}
-
-	// run the command
-	err = commands.run(&s, cmd)
-	if err != nil {
-		log.Fatal(err)
-	}
+	commands.Init()
 }
