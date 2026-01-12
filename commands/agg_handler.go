3ae6ddc8c9381b163c84fb572aad2e81c03efb6f
Khizar Nawab <khizarnawab1@outlook.com>
2026-01-13 02:52:32 +0500

    feat: fetch feeds from different sources

--- commands/agg_handler.go
+++ commands/agg_handler.go
@@ -3,17 +3,46 @@ package commands
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/khizar-sudo/feed-aggregator/feed"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	feed, err := feed.FetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("Provide a time duration!")
+	} else if len(cmd.args) > 1 {
+		return fmt.Errorf("Too many arguments")
+	}
+
+	timeDuration, err := time.ParseDuration(cmd.args[0])
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("%+v\n", feed)
+	fmt.Printf("Collecting feeds every %v\n", timeDuration)
+	ticker := time.NewTicker(timeDuration)
+	for ; ; <-ticker.C {
 
-	return nil
+		feedToFetch, err := s.db.GetNextFeedToFetch(context.Background())
+		if err != nil {
+			return err
+		}
+
+		_, err = s.db.MarkFeedFetched(context.Background(), feedToFetch.ID)
+		if err != nil {
+			return err
+		}
+
+		f, err := feed.FetchFeed(context.Background(), feedToFetch.Url)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Channel: %s\n", f.Channel.Title)
+		for _, item := range f.Channel.Item {
+			fmt.Printf("* %s\n", item.Title)
+		}
+		fmt.Println("=====================================")
+	}
 }
